5a0e2ffb12e10b5b5ebd48fb095c15390930729e
Sebastian Messingfeld <sebastian@messeb.com>
2024-01-30 23:20:21 +0100

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,25 +1,21 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "1.0.0"
-let moduleName = "shared"
-let checksum = "96af12badd937facc56058384ab07f9127f91ef17b33fa728e106419caacb2e2"
-
 let package = Package(
-    name: moduleName,
+    name: "shared",
     platforms: [
-		.iOS(.v16)
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: moduleName,
-            targets: [moduleName]
-        )
+            name: "shared",
+            targets: ["shared"]
+        ),
     ],
     targets: [
         .binaryTarget(
-        	name: moduleName
-            url: "https://github.com/trashcan-xyz/kotlin-mobile-api/raw/main/package/shared.framework",
-        )
+            name: "shared",
+            path: "./package/shared.framework"
+        ),
     ]
 )
